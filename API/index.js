dd6e2b3aeabfeeaa6f5cd24dc3abe588c58027df
SebastienBtr <sebastien29520@gmail.com>
2018-04-28 10:29:27 +0200

    initial commit

--- API/index.js
+++ API/index.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 app.use((req, res, next) => {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'localhost, sharebest.online');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
